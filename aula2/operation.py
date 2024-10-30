01f5d8f87539e5d7e2bacd627353302ac65ef26c
Sarah Nicacio <nicaciosarah085@gmail.com>
2024-10-30 19:46:58 -0300

    operacoes

---
+++ aula2/operation.py
@@ -0,0 +1,19 @@
+operacao = input()
+int1 = int(input())
+int2 = int(input())
+
+if (operacao == "+"):
+    print(int1 + int2)
+elif (operacao == "-"):
+    print(int1 - int2)
+elif (operacao == "/"):
+    print(int1 / int2)
+elif (operacao == "*"):
+    print(int1 * int2)
+elif (operacao == "//"):
+    print(int1 // int2)
+elif (operacao == "%"):
+    print(int1 % int2)
+elif (operacao == "**"):
+    print(int1 ** int2)
+    
